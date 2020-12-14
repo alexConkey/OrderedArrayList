0118c4308fac98e85406912d0fc32c8441129307
Alex Conkey <aconkey20@stuy.edu>
2020-12-14 10:47:21 -0500

    Fixed some problems

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,22 +1,24 @@
-public class OrderedArrayList<T> extends Comparable<T> extends NoNullArrayList<T> {
+import java.util.ArrayList;
+public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
   public OrderedArrayList(){
     super();
   }
-  public OrderedArrayList(startingCapacity){
+  public OrderedArrayList(int startingCapacity){
     super(startingCapacity);
   }
   public boolean add(T t){
-    for(int i = 0; i < super.size(); i++){
-      if (super.get(i)>t){
-        return(super.add(i, t));
+    for(int i = 0; i < size(); i++){
+      if (get(i).compareTo(t) >= 0){
+        super.add(i, t);
+        return true;
       }
     }
     return(super.add(t));
   }
   public void add(int index, T t){
     Boolean added = false;
-    for(int i = 0; i < super.size(); i++){
-      if (super.get(i)>t){
+    for(int i = 0; i < size(); i++){
+      if (get(i).compareTo(t) >= 0){
         super.add(i, t);
         added = true;
       }
@@ -26,7 +28,10 @@ public class OrderedArrayList<T> extends Comparable<T> extends NoNullArrayList<T
     }
   }
   public T set(int index, T t){
-    super.remove(index);
+    T oldValue = get(index);
+    remove(index);
     add(t);
+    return oldValue;
+
   }
 }
