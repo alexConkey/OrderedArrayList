47a76f19775b623b602519e9301f234359273537
Alex Conkey <aconkey20@stuy.edu>
2020-12-14 11:11:54 -0500

    made IllegalArgumentExceptions for OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -7,31 +7,43 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
   public boolean add(T t){
-    for(int i = 0; i < size(); i++){
-      if (get(i).compareTo(t) >= 0){
-        super.add(i, t);
-        return true;
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be null");
+    }else{
+      for(int i = 0; i < size(); i++){
+        if (get(i).compareTo(t) >= 0){
+          super.add(i, t);
+          return true;
+        }
       }
+      return(super.add(t));
     }
-    return(super.add(t));
   }
   public void add(int index, T t){
-    Boolean added = false;
-    for(int i = 0; i < size(); i++){
-      if (get(i).compareTo(t) >= 0){
-        super.add(i, t);
-        added = true;
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be null");
+    }else{
+      Boolean added = false;
+      for(int i = 0; i < size(); i++){
+        if (get(i).compareTo(t) >= 0){
+          super.add(i, t);
+          added = true;
+        }
       }
-    }
-    if (! added){
-      super.add(t);
+      if (! added){
+        super.add(t);
+      }
     }
   }
   public T set(int index, T t){
-    T oldValue = get(index);
-    remove(index);
-    add(t);
-    return oldValue;
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be null");
+    }else{
+      T oldValue = get(index);
+      remove(index);
+      add(t);
+      return oldValue;
+    }
 
   }
 }
