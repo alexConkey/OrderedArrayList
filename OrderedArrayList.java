7f7c77fbd242104d77cff3da7da5a0fba146b143
Alex Conkey <>
2020-12-14 09:45:41 -0500

    first add done

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -5,4 +5,15 @@ public class OrderedArrayList<T> extends Comparable<T> extends NoNullArrayList<T
   public OrderedArrayList(startingCapacity){
     super(startingCapacity);
   }
+  public boolean add(T t){
+    for(int i = 0; i < super.size(); i++){
+      if (super.get(i)>t){
+        return(super.add(i, t));
+      }
+    }
+    return(super.add(t));
+  }
+  public void add(int index, T t){
+    
+  }
 }
