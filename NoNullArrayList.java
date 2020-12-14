acdfa24d97f0a06068ac26a16c8a25fc3ef05d50
Alex Conkey <aconkey20@stuy.edu>
2020-12-14 08:44:56 -0500

    finished both adds and set

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -6,12 +6,24 @@ public class NoNullArrayList<T> extends Arraylist<T> {
 
   }
   public boolean add(T t){
-
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be null");
+    }else{
+      super.add(t);
+    }
   }
   public void add(int index, T element){
-
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be null");
+    }else{
+      super.add(index, t);
+    }
   }
   public void set(T t){
-    
+    if (t == null){
+      throw new IllegalArgumentException("Value cannot be null");
+    }else{
+      super.set(t);
+    }
   }
 }
